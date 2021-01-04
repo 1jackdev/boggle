bf405fd8a91a0c92b3136d13eae036524d404ca2
Jack <jackmayer21@gmail.com>
2021-01-03 16:15:34 -0800

    add flask

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 from boggle import Boggle
+from flask import Flask, request, render_template
 
 boggle_game = Boggle()
+
+
+@app.route('/')
+def show_homepage():
+   return render_template("index.html")
