ea187c60d144ee2c8e518b09efa5dcc42ac0ba1c
Jack <jackmayer21@gmail.com>
2021-01-03 19:03:54 -0800

    added timer and messages

--- app.py
+++ app.py
@@ -10,16 +10,32 @@ boggle_game = Boggle()
 
 @app.route('/')
 def show_homepage():
+    """Show the board and its score data."""
     board = boggle_game.make_board()
     session["board"] = board
-    return render_template("index.html", board=board)
+    highscore = session.get("highscore", 0)
+    num_plays = session.get("num-plays", 0)
+    return render_template("index.html", board=board, highscore=highscore, num_plays=num_plays)
 
 
 @app.route('/make-guess')
 def handle_guess():
+    """take the guess from the board and check it. Then, return the result."""
     board = session["board"]
     guess = request.args["guess"]
     result = boggle_game.check_valid_word(board, guess)
     return jsonify({"result": result})
-    
-    
+
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    """Receive score, update num_plays, and update the high score."""
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    num_plays = session.get("num_plays", 0)
+
+    session['num_plays'] = num_plays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
--- static/app.js
+++ static/app.js
@@ -1,10 +1,12 @@
 class BoggleGame {
     constructor(boardId, secs = 60) {
         this.secs = secs;
-        // this.showTimer();
+        this.showTimer();
         this.score = 0;
         this.words = new Set();
         this.board = "#" + boardId;
+
+        this.timer = setInterval(this.tick.bind(this), 1000);
 
         $(".guess-form", this.board).on("submit", this.processForm.bind(this));
     }
@@ -15,6 +17,14 @@ class BoggleGame {
 
     showMessage(msg, cls) {
         $(".msg", this.board).text(msg).removeClass().addClass(`msg ${cls}`);
+    }
+
+    showScore() {
+        $(".score", this.board).text(this.score);
+    }
+
+    showTimer() {
+        $(".timer", this.board).text(this.secs);
     }
 
     async processForm(evt) {
@@ -36,15 +46,38 @@ class BoggleGame {
 
         if (resp.data.result === "not-word") {
             this.showMessage(`${guess} is not a valid English word`, "err");
-          } else if (resp.data.result === "not-on-board") {
-            this.showMessage(`${guess} is not a valid word on this board`, "err");
-          } else {
+        } else if (resp.data.result === "not-on-board") {
+            this.showMessage(
+                `${guess} is not a valid word on this board`,
+                "err"
+            );
+        } else {
             this.showWord(guess);
             this.score += guess.length;
             this.showScore();
             this.words.add(guess);
             this.showMessage(`Added: ${guess}`, "ok");
-          }
+        }
         $("#guess-input").val("").focus();
+    }
+
+    async tick() {
+        this.secs -= 1;
+        this.showTimer();
+
+        if (this.secs === 0) {
+            clearInterval(this.timer);
+            await this.scoreGame();
+        }
+    }
+
+    async scoreGame() {
+        $(".add-word", this.board).hide();
+        const resp = await axios.post("/post-score", { score: this.score });
+        if (resp.data.brokeRecord) {
+            this.showMessage(`New record: ${this.score}`, "ok");
+        } else {
+            this.showMessage(`Final score: ${this.score}`, "ok");
+        }
     }
 }
