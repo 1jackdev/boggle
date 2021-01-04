f32b3172fc8f7b0a408eeb190bf27c62f20aa725
Jack <jackmayer21@gmail.com>
2021-01-03 17:32:59 -0800

    board and form html

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from boggle import Boggle
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "boggle1"
 
 boggle_game = Boggle()
 
 
 @app.route('/')
 def show_homepage():
-   return render_template("index.html")
+    board = boggle_game.make_board()
+    session["board"] = board
+    return render_template("index.html", board=board)
